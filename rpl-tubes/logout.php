de9ffa55f7cbabc2d522094298229b7e456fb13c
Yosafat <77567907+crypter7054@users.noreply.github.com>
2022-05-25 12:06:56 +0700

    Delete rpl-tubes directory

--- rpl-tubes/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-session_start();                // Start sessionnya
-session_destroy();              // Hapus semua session
-header("location: index.php");  // Redirect ke halaman index.php
-
-
-?>
